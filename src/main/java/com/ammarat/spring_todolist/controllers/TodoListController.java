c5f9195ef2eea8c495b6bba2b094703807b8ed58
ibrahimabdouammarat <ibrahimabdouammarat2gmail.com>
2026-01-01 13:09:14 +0100

    feat/entity-repository

--- src/main/java/com/ammarat/spring_todolist/controllers/TodoListController.java
+++ src/main/java/com/ammarat/spring_todolist/controllers/TodoListController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/todolist")
 public class TodoListController {
 
     private final TodoListService todoListService;
