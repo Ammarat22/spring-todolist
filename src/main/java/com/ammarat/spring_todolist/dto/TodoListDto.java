a65976172b9545027a6e52864b11ba24c68c6bb5
ibrahimabdouammarat <ibrahimabdouammarat2gmail.com>
2026-01-05 13:00:10 +0100

    fix/entity-dto

--- src/main/java/com/ammarat/spring_todolist/dto/TodoListDto.java
+++ src/main/java/com/ammarat/spring_todolist/dto/TodoListDto.java
@@ -2,6 +2,8 @@ package com.ammarat.spring_todolist.dto;
 
 
 import lombok.Data;
+
+import java.time.Instant;
 import java.time.LocalDateTime;
 
 @Data
@@ -9,7 +11,7 @@ public class TodoListDto {
     private Integer id;
     private String title;
     private String description;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+    private Instant createdAt;
+    private Instant updatedAt;
 }
 
